0eba7b592aa6f05e0f363252ebe015fca8c12002
Heather Battaglia <heather.battaglia@gsa.gov>
2016-04-19 15:46:18 -0500

    create structured data from user input

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 function inputToSessionStorage(){
+  function verifyOrCreateKey(el, key){
+    if (!tempObj[key]){
+      tempObj[key] = {};
+    }
+    return tempObj;
+  }
+  var tempObj = {};
   try {
     [].forEach.call(document.querySelectorAll('input, select'), function(el){
+      var key = el.attributes["data-key"].value;
+      verifyOrCreateKey(el, key);
       if (el.value){
-        //TODO: Add ids to forms so we can have some better hierarchy
-        sessionStorage.setItem(el.id, el.value);
+        tempObj[key][el.id] = el.value;
       }
+      // Attach user input, now stored in tempObj, to the correct sessionStorage key
+      // Can later be parsed like so: JSON.parse(sessionStorage.payroll_start_week)
+      sessionStorage.setItem(key, JSON.stringify(tempObj[key]));
     });
   } catch (e){
     console.log("No values found.");
@@ -14,7 +25,6 @@ function inputToSessionStorage(){
 // Add click listener to buttons
 [].forEach.call(document.getElementsByClassName('usa-button'), function(el){
   // Only add the event listener to capture input if user didn't click cancel.
-  // NOTE: Cancel needs to be added to cancel buttons still.
   if (el.className.indexOf('cancel') == -1) {
     el.addEventListener('click', function(evt) {
       inputToSessionStorage();
